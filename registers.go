e791a59885489318ce3baa0d51dfda26cbd6ee4c
Steven Anderson <sdandersonz@icloud.com>
2021-11-23 23:09:30 -0800

    few more instructions

--- registers.go
+++ registers.go
@@ -3,11 +3,13 @@ import (
     "time"
 )
 type registers struct {
-    GP [16] byte
+    GP [16] byte //vx -> x == idx
     I uint16
     PC uint16
     SP uint16
     DT byte
+    ST byte
+    soundBuffer *bool
 }
 func InitRegisters () *registers {
     regs := new(registers)
@@ -16,7 +18,74 @@ func InitRegisters () *registers {
     regs.SP = 0
     return regs
 }
-
+func (r *registers) WriteVx(vx uint8, value byte) {
+    r.GP[vx] = value 
+}
+func (r *registers) ReadVx(vx uint8) byte {
+    return r.GP[vx] 
+}
+func (r *registers) AddToVx(vx uint8, value byte) {
+    r.GP[vx] += value
+}
+func (r *registers) MoveVyToVx(vy uint8, vx uint8){
+    r.GP[vx] = r.GP[vy]
+}
+func (r *registers) OrVxVy(vx uint8, vy uint8) {
+     r.GP[vx] = r.GP[vx] | r.GP[vy]
+}
+func (r* registers) AndVxVy(vx uint8, vy uint8)  {
+    r.GP[vx] = r.GP[vx] & r.GP[vy]
+}
+func (r* registers) XOrVxVy (vx uint8, vy uint8)  {
+     r.GP[vx] = r.GP[vx] ^  r.GP[vy]
+}
+func (r* registers) AddVyVx (vy uint8, vx uint8)  {
+    sum := uint16(r.GP[vx]) + uint16(r.GP[vy]) 
+    //if overflows carry 1 to VF
+    if sum > 255{
+        r.GP[15] = 1
+        r.GP[vx] = sum && 0xFF
+    } else {
+        r.GP[vx] += r.GP[vy]
+    }
+}
+func (r* registers) SubVyVx (vy uint8, vx uint8) {
+    if r.GP[vx] > r.GP[vy] {
+        r.GP[15] = 1
+    } else {
+        r.GP[15] = 0
+    }
+    r.GP[vx] = r.GP[vx] - r.GP[vy]
+}
+func (r *registers) ShiftRVx (vx uint8) {
+    if r.GP[vx] && 0x01 == 1 {
+        r.GP[15] = 1
+    } else {
+        r.GP[15] = 0
+    }
+    r.GP[vx] = r.GP[vx] >> 1
+}
+func (r *registers) SubNVxVx (vx uint8, vy uint8) {
+    if vy > vx {
+        r.GP[15] = 1
+    } else {
+        r.GP[15] = 0
+    }
+    r.GP[vx] = r.GP[vy] r.GP[vx]
+}
+func (r *registers) ShiftLVx (vx uint8) {
+    if r.GP[vx] && 0x1 {
+        r.GP[15] = 1
+    } else {
+        r.GP[15] = 0
+    }
+    r.GP[vx] = r.GP[vx] << 1
+}
+func (r *registers) SkipNextInstruction (vx uint8, vy uint8) {
+    if r.GP[vx] != r.GP[vy] {
+        r.PC = r.PC << 1
+    }
+}
 func (regs *registers) UpdateClockTimers () {
     if regs.DT > 0 {
         regs.DT--

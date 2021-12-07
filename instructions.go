1e50b4d5d37e4f371a7689f3b9a1ae2e1fc6143d
Steven Anderson <sdandersonz@icloud.com>
2021-12-07 08:44:22 -0800

    Draw instrusccion

--- instructions.go
+++ instructions.go
@@ -26,6 +26,31 @@ func handleSystemInstruccion (instruccion uint16, p *cpu) {
                 p.display.Clear()
         }
 }
+func drawSprite (x uint8, y uint8, n uint8, p *cpu) {
+    vx := p.regs.ReadVx(x) & 63
+    vy := p.regs.ReadVx(y) & 31
+    i := p.regs.I
+    p.regs.GP[15] = 0
+    for j:=uint8(0); j < n; j++ {
+        spriteData:= p.m.ReadFromMemory(i)
+        mask:=byte(0x80)
+        for mask > 0 {
+            isBitOn := uint8(spriteData & mask) > 0
+            if isBitOn {
+                isPixelOn := p.display.screen[vy][vx]
+                if isPixelOn {
+                    p.display.screen[vy][vx] =false 
+                    p.regs.GP[15]=15
+                } else {
+                    p.display.screen[vy][vx] = true
+                }
+            }
+            vx += 1
+            mask = mask >> 1
+        }
+        vy +=1
+    }
+}
 func (p *cpu) Execute(opCode string) {
     sysCode, value := splitInstruccion(opCode)
     
@@ -37,20 +62,25 @@ func (p *cpu) Execute(opCode string) {
             p.regs.SetPC(value)
         case 0x6:
             // set Register VX
-   //         x := opCode[1:2]
- //           value := opCode[3:5]
-     //       p.regs.WriteVx(x, value)
-            break
+            x := uint8(value >> 8)
+            val := uint8(value)
+            p.regs.WriteVx(x, val)
         case 0x7:
             //add value to register VX
+            x := uint8(value >> 8)
+            val := uint8(value)
+            p.regs.AddToVx(x, val)
             break
         case 0xA: 
             // set index register I
-            address, err := strconv.ParseUint(opCode[1:4], 16, 32)
-            check(err)
-            p.regs.SetI(uint16(address))
+            p.regs.SetI(value)
         case 0xD:
             // display draw
+            x := uint8(value >> 8)
+            y := uint8((value & 0x0F) >> 4)
+            n := uint8((value & 0x00F))
+            
+            drawSprite(x,y,n,p)
             break
     }
 }
